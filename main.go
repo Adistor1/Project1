8f73e77d2fc67c0b1fd1540a7263241911e4377f
Adistor1 <139944289+Adistor1@users.noreply.github.com>
2023-07-20 15:38:37 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-	// Create an instance of Echo
+	
 	e := echo.New()
 
-	// Define a route handler for the root path "/"
+ 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Adi's First API code")
 	})
 
-	// Start the server
+	
 	e.Start(":8080")
 }
